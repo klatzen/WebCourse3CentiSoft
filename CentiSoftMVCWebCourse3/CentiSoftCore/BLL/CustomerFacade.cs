f234cd2ec477103eefb148e004792f2eb4ee3c6b
Kristine El-Banna <1054336@ucn.dk>
2017-03-08 11:55:26 +0100

    CustomerBLL

--- CentiSoftMVCWebCourse3/CentiSoftCore/BLL/CustomerFacade.cs
+++ CentiSoftMVCWebCourse3/CentiSoftCore/BLL/CustomerFacade.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CentiSoftCore.DAL;
+using CentiSoftCore.MODELS;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,37 @@ using System.Threading.Tasks;
 
 namespace CentiSoftCore.BLL
 {
-    class CustomerFacade : BaseFacade
+    public class CustomerFacade : BaseFacade
     {
+        private CustomerRepository customerRepository;
+        private ProjectRepository projectRepository;
+        public CustomerFacade(int clientId) : base(clientId)
+        {
+
+            customerRepository = new CustomerRepository();
+            projectRepository = new ProjectRepository();
+
+        }
+
+        public Customer loadCustomer(int id)
+        {
+            return customerRepository.loadCustomer(id);
+        }
+
+        public List<Customer> loadAllCustomers()
+        {
+            return customerRepository.LoadAllCustomers();
+        }
+
+        public void saveCustomer(Customer customer)
+        {
+            customerRepository.saveCustomer(customer);
+        }
+
+        public void deleteCustomer(int id) {
+
+            Customer cus = customerRepository.loadCustomer(id);
+            bool customerHasProj = projectRepository.hasTasks(id);
+        }
     }
 }
