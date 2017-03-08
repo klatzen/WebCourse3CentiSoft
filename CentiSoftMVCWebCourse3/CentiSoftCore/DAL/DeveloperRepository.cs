e09b85e7a22084f3be2c7279e3fb91b059b29829
darlenedilla <darlenedilla@hotmail.com>
2017-03-08 11:00:30 +0100

    tomme rep klasser

---
+++ CentiSoftMVCWebCourse3/CentiSoftCore/DAL/DeveloperRepository.cs
@@ -0,0 +1,18 @@
+﻿using CentiSoftCore.MODELS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentiSoftCore.DAL
+{
+    class DeveloperRepository : BaseRepository
+    {
+
+        public DeveloperRepository()
+        {
+            
+        }
+    }
+}
